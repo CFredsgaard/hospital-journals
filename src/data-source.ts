994bc6d1bccf2cce4e987f0d9eedb24831f6c2bc
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2024-04-29 17:40:46 +0200

    Add Entities to data-source
    Patient, Doctor, Admission, Department all added to the data-source file entities

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,10 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
+import { Patient } from "./entity/Patient"
+import { Doctor } from "./entity/Doctor"
+import { Admission } from "./entity/Admission"
+import { Department } from "./entity/Department"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -11,7 +15,7 @@ export const AppDataSource = new DataSource({
     database: "postgres",
     synchronize: true,
     logging: false,
-    entities: [User],
+    entities: [User, Patient, Doctor, Admission, Department],
     migrations: [],
     subscribers: [],
 })
