883368d756d09032ef400f3511085a55b6557a58
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2024-04-30 10:52:58 +0200

    host in db setting changed
    changed hostname to postgres

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,7 +7,7 @@ import { Department } from "./entity/Department"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "postgres",
     port: 5432,
     username: "postgres",
     password: "postgres",
